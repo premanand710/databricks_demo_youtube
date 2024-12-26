845eb587a163d1db905ccae3d296b1de3fa67193
premanand710 <premanand710>
2024-12-26 11:05:35 +0000

    Commit and push

---
+++ (Clone) CICD Notebook.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+# MAGIC  %sh
+# MAGIC  rm -r /dbfs/FileStore
+# MAGIC  mkdir /dbfs/FileStore
+# MAGIC  wget -O /dbfs/FileStore/sample_sales.csv https://github.com/MicrosoftLearning/mslearn-databricks/raw/main/data/sample_sales.csv
+
+# COMMAND ----------
+
+ from pyspark.sql.types import DateType, StructType, StructField, StringType, IntegerType
+
+schema = StructType([
+    StructField("Date",DateType()),
+    StructField("Product",StringType()),
+    StructField("Quantity",IntegerType()),
+    StructField("Price",IntegerType())])
+ 
+ df = spark.read.csv("/FileStore/sample_sales.csv",schema=schema,header=True)
+
+ display(df)
+ df.printSchema()
+
+# COMMAND ----------
+
+df.schema
